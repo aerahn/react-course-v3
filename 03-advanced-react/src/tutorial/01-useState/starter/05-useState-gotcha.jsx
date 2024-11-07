7714123e7bfde3abbc9a15dc82c53c37ea7141bc
Amanda Wu <awu.amanda@gmail.com>
2024-11-07 21:16:16 +0000

    Tutorial Updates

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,25 @@
+import React from "react";
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [number, setNumber] = useState(0);
+
+  const handleClick = () => {
+    setTimeout(() => {
+      setNumber((currentState) => {
+        return currentState + 1;
+      });
+    }, 3000);
+  };
+
+  return (
+    <div>
+      <h2>{number}</h2>
+      <button type="button" onClick={handleClick}>
+        Increase
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
