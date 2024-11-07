90e9b9c98349ef031412b054538e1ae3f184f900
Amanda Wu <awu.amanda@gmail.com>
2024-11-07 20:27:37 +0000

    Updated tutorials

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,11 @@
+import Starter from "./tutorial/01-useState/starter/05-useState-gotcha.jsx";
+import Final from "./tutorial/01-useState/final/05-useState-gotcha.jsx";
+
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced React</h2>
+    <div className="container">
+      <Starter />
+      {/* <Final /> */}
     </div>
   );
 }
