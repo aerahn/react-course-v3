a9034ad2c496bffd39e993add42c525f5d393744
Amanda Wu <awu.amanda@gmail.com>
2024-11-08 19:56:22 +0000

    Updated Tutorial

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,12 @@
+import {Starter} from './tutorial/02-useEffect/starter/01-code-example';
+import {Final} from './tutorial/02-useEffect/final/01-code-example';
+
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced React</h2>
+      <Starter />
+      <Final />
     </div>
   );
 }
